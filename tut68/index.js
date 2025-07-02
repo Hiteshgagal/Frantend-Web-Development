0073002f446a4be39d63996ccda788d1ca2c076f
Hiteshgagal <hiteshgagal5@gmail.com>
2025-07-02 18:21:15 +0530

    Add files via upload

---
+++ tut68/index.js
@@ -0,0 +1,7 @@
+//console.log(average([4,5]));
+//const average = require("./mod");
+const mod = require("./mod");
+console.log(mod.name)
+console.log(mod.repo)
+//console.log(mod.avg([3,4]))
+console.log("This is index.js");
